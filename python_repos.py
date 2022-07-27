9b7edbc2e8deed75f2e9a9fa650d84bbba3db223
Abe Ziady <abraham.ziady@gmail.com>
2022-07-27 16:14:26 -0400

    Using PYGAL

--- python_repos.py
+++ python_repos.py
@@ -1,4 +1,7 @@
 import requests
+import pygal
+from pygal.style import LightColorizedStyle as LCS, LightStyle as LS
+
 
 # Make an API call and store the responses.
 url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
@@ -14,7 +17,8 @@ print("Total repositories: ", response_dict['total_count'])
 
 # Explore information about the repositories.
 repo_dicts = response_dict['items']
-print("Repositories returned: ", len(repo_dicts))
+names, stars = [], []
+# print("Repositories returned: ", len(repo_dicts))
 
 # # Examine the first repository.
 # repo_dict = repo_dicts[0]
@@ -30,10 +34,22 @@ print("Repositories returned: ", len(repo_dicts))
 # print('Updated: ', repo_dict['updated_at'])
 # print('Description: ', repo_dict['description'])
 
-print("\nSelected information about each repository: ")
+# print("\nSelected information about each repository: ")
 for repo_dict in repo_dicts:
-    print("\nName: ", repo_dict['name'])
-    print('Owner: ', repo_dict['owner']['login'])
-    print('Stars: ', repo_dict['stargazers_count'])
-    print('Repository: ', repo_dict['html_url'])
-    print('Description: ', repo_dict['description'])
+    names.append(repo_dict['name'])
+    stars.append(repo_dict['stargazers_count'])
+
+# Make visualization.
+# my_style = LS('#333366', base_style=LCS)
+my_style = LS()
+chart = pygal.Bar(style=my_style, x_label_rotation=45, show_legend=False)
+chart.title = 'Most-Starred Python Projects on Github'
+chart.x_labels = names
+
+chart.add('', stars)
+chart.render_to_file('python_repos.svg')
+#     print("\nName: ", repo_dict['name'])
+#     print('Owner: ', repo_dict['owner']['login'])
+#     print('Stars: ', repo_dict['stargazers_count'])
+#     print('Repository: ', repo_dict['html_url'])
+#     print('Description: ', repo_dict['description'])
